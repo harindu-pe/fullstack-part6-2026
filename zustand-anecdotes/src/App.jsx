133798a1dc412f52cc57f8611bc5b3fffb18ad6f
harindu-pe <harindu.pe@gmail.com>
2026-05-15 19:24:11 +0530

    exercise 6.2 completed

--- zustand-anecdotes/src/App.jsx
+++ zustand-anecdotes/src/App.jsx
@@ -1,17 +1,17 @@
-
-import { useAnecdotes } from './store'
+import { useAnecdoteActions, useAnecdotes } from "./store";
 
 const App = () => {
-  const anecdotes = useAnecdotes()
+  const anecdotes = useAnecdotes();
+  const actions = useAnecdoteActions();
 
-  const vote = id => {
-    console.log('vote', id)
-  }
+  const vote = (id) => {
+    actions.vote(id);
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
@@ -28,7 +28,7 @@ const App = () => {
         <button>create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
