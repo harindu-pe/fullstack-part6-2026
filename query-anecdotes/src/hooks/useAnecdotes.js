2f1d11063bf1a7d65190c09ce753a06fdd2cc4be
harindu-pe <harindu.pe@gmail.com>
2026-05-22 15:29:44 +0530

    exercise 6.21 completed

--- query-anecdotes/src/hooks/useAnecdotes.js
+++ query-anecdotes/src/hooks/useAnecdotes.js
@@ -1,8 +1,11 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getAnecdotes, createAnecdote, updateAnecdote } from "../requests";
+import useNotification from "./useNotification";
 
 export const useAnecdotes = () => {
   const queryClient = useQueryClient();
+
+  const { triggerNotification } = useNotification();
 
   const result = useQuery({
     queryKey: ["anecdotes"],
@@ -16,12 +19,18 @@ export const useAnecdotes = () => {
       const anecdotes = queryClient.getQueryData(["anecdotes"]);
       queryClient.setQueryData(["anecdotes"], anecdotes.concat(newAnecdote));
     },
+    onError: (error) => {
+      triggerNotification("too short anecdote, must have length 5 or more", 5);
+    },
   });
 
   const updateAnecdoteMutation = useMutation({
     mutationFn: updateAnecdote,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+    onError: (error) => {
+      console.error("Error updating anecdote:", error);
     },
   });
 
