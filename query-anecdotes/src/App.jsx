2039af36be8fd73376a24bfbf5738ffddc311569
harindu-pe <harindu.pe@gmail.com>
2026-05-22 14:22:13 +0530

    exercise 6.18 completed

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,13 +1,21 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { useQuery } from "@tanstack/react-query";
-import { getAnecdotes } from "./requests";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
   const result = useQuery({
     queryKey: ["anecdotes"],
     queryFn: getAnecdotes,
     retry: false,
+  });
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
   });
 
   if (result.isPending) {
@@ -21,7 +29,7 @@ const App = () => {
   const anecdotes = result.data;
 
   const handleVote = (anecdote) => {
-    console.log("vote");
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
   return (
