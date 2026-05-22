4e80a2852cfa410affc56c9c201b379579d3bb06
harindu-pe <harindu.pe@gmail.com>
2026-05-22 14:15:24 +0530

    exercise 6.17 completed

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,17 +1,12 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 import { useQuery } from "@tanstack/react-query";
+import { getAnecdotes } from "./requests";
 
 const App = () => {
   const result = useQuery({
     queryKey: ["anecdotes"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3001/anecdotes");
-      if (!response.ok) {
-        throw new Error("Failed to fetch anecdotes");
-      }
-      return await response.json();
-    },
+    queryFn: getAnecdotes,
     retry: false,
   });
 
